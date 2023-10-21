6ae71b4a685968f32f77f8d6c6bbbd31f3d85472
MarkNdirangu <itsmaccc@gmail.com>
2023-10-21 12:11:27 +0300

    new commit

--- quiz.js
+++ quiz.js
@@ -38,7 +38,7 @@ const questions = [
     },
 ];
 const questionElement = document.getElementById("question");
-const answerButton = document.getElementById("answer-buttons");
+const answerButtons = document.getElementById("answer-buttons");
 const nextButton = document.getElementById("next-btn");
 
 let currentQuestionIndex = 0;
@@ -54,10 +54,28 @@ function startQuiz(){
 }
 
 function showQuestion(){
+    resetState();
     let currentQuestion = questions[currentQuestionIndex];
     let questionNo = currentQuestionIndex + 1;
     questionElement.innerHTML = questionNo + ". " + currentQuestion.
     questions;
 
-
+    currentQuestion.answers.forEach(answer => {
+        const button= document.createElement("button");
+        button.innerHTML = answer.text;
+        button.classList.add("btn");
+        answerButtons.appendChild(button);
+         
+    });
 }
+function resetState(){
+    nextButton.style.display = "none";
+    while(answerButtons.firstChild){
+        answerButtons.removeChild(answerButtons.firstChild);
+
+
+    }
+}
+
+ startQuiz();
+  
